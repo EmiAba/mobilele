d8debda43b140059967c50c5c6b469810f44b4a8
EmiAba <106099092+EmiAba@users.noreply.github.com>
2023-11-27 16:23:53 +0200

    Java Project

---
+++ src/main/java/org/softuni/mobilele/init/DBInit.java
@@ -0,0 +1,24 @@
+package org.softuni.mobilele.init;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DBInit implements CommandLineRunner {
+
+    private final String defaultAdminPass;
+
+    public DBInit(@Value("${default.admin.pass}") String defaultAdminPass) {
+
+        this.defaultAdminPass = defaultAdminPass;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        //todo
+
+        //System.out.println("Default admin pass: " + defaultAdminPass);
+
+    }
+}
