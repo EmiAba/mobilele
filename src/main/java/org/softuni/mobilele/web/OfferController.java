3e3e95b23698931b30a02e9a0d1cdb6b06bbb2b2
EmiAba <abadjieva@gmail.com>
2023-12-03 19:42:53 +0200

    Java-Web

--- src/main/java/org/softuni/mobilele/web/OfferController.java
+++ src/main/java/org/softuni/mobilele/web/OfferController.java
@@ -1,12 +1,11 @@
 package org.softuni.mobilele.web;
 
 import org.softuni.mobilele.model.dto.CreateOfferDto;
+import org.softuni.mobilele.model.enums.EngineEnum;
 import org.softuni.mobilele.service.OfferService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -26,31 +25,33 @@ public class OfferController {
         return "offers";
     }
 
+    @ModelAttribute("engines")
+    public EngineEnum[] engines() {
+        return EngineEnum.values();
+
+    }
+
+
     @GetMapping("/add")
-    public String add() {
+    public String add(Model model) {
 
         return "offer-add";
     }
 
 
     @PostMapping()
-    public String add(CreateOfferDto createOfferDto){
-                offerService.createOffer(createOfferDto);
+    public String add(CreateOfferDto createOfferDto) {
+        offerService.createOffer(createOfferDto);
 
         return "index";
 
     }
 
 
-
     @GetMapping("/{uuid}/details")
     public String details(@PathVariable("uuid") UUID uuid) {
         return "details";
     }
-
-
-
-
 
 
 }
